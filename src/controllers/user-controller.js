0b81ce114a7abac25fcc3563bb235698e4f465b6
Deepak <deepaksingh128367@gmail.com>
2024-01-21 17:52:36 +0530

    Added new user signup api

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,32 @@
+const { StatusCodes } = require('http-status-codes');
+const { UserService } = require('../services');
+
+const { SuccessResponse, ErrorResponse} = require('../utils/common');
+
+/**
+ * POST : /signup
+ * req-body  {email : 'deep@gmail.com', password: '1234'}
+ * 
+ */
+async function signup(req, res) {
+    try {
+        const user = await UserService.create({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+                .status(StatusCodes.CREATED) 
+                .json(SuccessResponse);
+
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res
+                .status(error.statusCode)
+                .json(ErrorResponse)
+    }
+}
+
+module.exports = {
+    signup    
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 const { InfoController } = require('../../controllers');  // requiring the controller (i.e. last middleware) 
        // do not forget to destructure the above thing
-       
+const userRouter = require('./user-routes');
+
 router.get('/info', InfoController.info);
 
-
+router.use('/signup', userRouter);
 module.exports = router;
